88bf3b963be8b9de5262672eaa4d42c649d6abd6
vinayakthakar <vinayak.thakar@gmail.com>
2021-01-11 18:46:42 +0530

    Commit 1

--- qaClick/src/test/java/qaClick/openWebPage.java
+++ qaClick/src/test/java/qaClick/openWebPage.java
@@ -3,6 +3,8 @@ package qaClick;
 import java.io.IOException;
 
 import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 import pageObjects.accessMenus;
@@ -12,22 +14,39 @@ public class openWebPage extends initBrowser{
 	
 	public WebDriver driver;
 	
+	@BeforeTest
+	public void openWeb() throws IOException
+	{
+		
+		driver = initializeBrowser();
+		driver.get(prop.getProperty("url"));
+		
+	}
+	
+	
 	@Test
 	public void accessWebPage() throws IOException
 	{
-		driver = initializeBrowser();
-		driver.get(prop.getProperty("url"));
+		
 		
 		accessMenus am = new accessMenus(driver);
 		
 		am.gethome().click();
-		am.getcourses().click();
+		/*am.getcourses().click();
 		am.getvideos().click();
 		am.getinterviewGuide().click();
 		am.getPractice().click();
 		am.getBlog().click();
 		am.getAbout().click();
-		am.getContact().click();
+		am.getContact().click();*/
+		
+		
+		
+	}
+	
+	@AfterTest
+	public void closeDriver()
+	{
 		
 		driver.close();
 		
